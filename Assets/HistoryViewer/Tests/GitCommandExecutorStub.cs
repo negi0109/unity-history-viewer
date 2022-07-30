b0cc5e832793a527ade2e983344246fce833fc42
negi0109 <33025461+negi0109@users.noreply.github.com>
2022-07-30 14:21:08 +0900

    fix TestRunner向けにTestフォルダの階層を少なくする

--- Assets/HistoryViewer/Tests/GitCommandExecutorStub.cs
+++ Assets/HistoryViewer/Tests/GitCommandExecutorStub.cs
@@ -5,34 +5,31 @@ using System.Text;
 using System.Text.RegularExpressions;
 using Negi0109.HistoryViewer.Interfaces;
 
-namespace Negi0109.HistoryViewer.Test
+public class GitCommandExecutorStub : IGitCommandExecutor
 {
-    public class GitCommandExecutorStub : IGitCommandExecutor
+    private readonly Dictionary<string, string> _dic;
+
+    public GitCommandExecutorStub(Dictionary<string, string> dic)
     {
-        private readonly Dictionary<string, string> _dic;
+        _dic = dic;
+    }
 
-        public GitCommandExecutorStub(Dictionary<string, string> dic)
+    public string ExecGitCommand(string arguments)
+    {
+        foreach (var pattern in _dic.Keys)
         {
-            _dic = dic;
+            if (Regex.IsMatch(arguments, pattern)) return _dic[pattern];
         }
 
-        public string ExecGitCommand(string arguments)
-        {
-            foreach (var pattern in _dic.Keys)
-            {
-                if (Regex.IsMatch(arguments, pattern)) return _dic[pattern];
-            }
+        throw new System.Exception();
+    }
 
-            throw new System.Exception();
-        }
+    public void ExecGitCommand(string arguments, Action<StreamReader> func)
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(ExecGitCommand(arguments)));
+        var stubReader = new StreamReader(stream);
 
-        public void ExecGitCommand(string arguments, Action<StreamReader> func)
-        {
-            var stream = new MemoryStream(Encoding.UTF8.GetBytes(ExecGitCommand(arguments)));
-            var stubReader = new StreamReader(stream);
-
-            func(stubReader);
-            stubReader.Close();
-        }
+        func(stubReader);
+        stubReader.Close();
     }
 }
--- Assets/HistoryViewer/Tests/GitLogLoaderTest.cs
+++ Assets/HistoryViewer/Tests/GitLogLoaderTest.cs
@@ -2,25 +2,22 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Negi0109.HistoryViewer.Models;
 
-namespace Negi0109.HistoryViewer.Test
+public class GitLogLoaderTest
 {
-    public class GitLogLoaderTest
+    [TestCase("a1", "commit1", "a2", "commit2")]
+    public void LoadTest(string commit1id, string commit1name, string commit2id, string commit2name)
     {
-        [TestCase("a1", "commit1", "a2", "commit2")]
-        public void LoadTest(string commit1id, string commit1name, string commit2id, string commit2name)
-        {
-            var git = new GitCommandExecutorStub(new Dictionary<string, string>
+        var git = new GitCommandExecutorStub(new Dictionary<string, string>
         {
             { "log.* --pretty=\"%H%x09%s\".* -- path", $"{commit1id}\t{commit1name}\n{commit2id}\t{commit2name}\n" }
         });
-            var loader = new GitLogLoader(git);
-            var commits = loader.Load("path");
+        var loader = new GitLogLoader(git);
+        var commits = loader.Load("path");
 
-            Assert.That(commits.Count, Is.EqualTo(2));
-            Assert.That(commits[0].hashId, Is.EqualTo(commit1id));
-            Assert.That(commits[0].name, Is.EqualTo(commit1name));
-            Assert.That(commits[1].hashId, Is.EqualTo(commit2id));
-            Assert.That(commits[1].name, Is.EqualTo(commit2name));
-        }
+        Assert.That(commits.Count, Is.EqualTo(2));
+        Assert.That(commits[0].hashId, Is.EqualTo(commit1id));
+        Assert.That(commits[0].name, Is.EqualTo(commit1name));
+        Assert.That(commits[1].hashId, Is.EqualTo(commit2id));
+        Assert.That(commits[1].name, Is.EqualTo(commit2name));
     }
 }
