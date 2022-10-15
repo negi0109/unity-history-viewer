dc326d254ecb973ade88637660bae5441b7c7622
negi0109 <33025461+negi0109@users.noreply.github.com>
2022-10-15 17:11:38 +0900

    add GUIDDatabase

---
+++ Assets/HistoryViewer/Scripts/models/GUIDDatabase/GUIDDatabase.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Negi0109.HistoryViewer.Models
+{
+    public class GUIDDatabase
+    {
+        public Dictionary<string, string> dic = new();
+    }
+}
