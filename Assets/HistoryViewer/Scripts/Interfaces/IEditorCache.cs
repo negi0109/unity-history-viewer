295354a159127f70de62e98e9fa36254814aa016
negi0109 <33025461+negi0109@users.noreply.github.com>
2022-10-15 16:31:01 +0900

    add EditorCache でStreamReaderでの読み取りを宣言

--- Assets/HistoryViewer/Scripts/Interfaces/IEditorCache.cs
+++ Assets/HistoryViewer/Scripts/Interfaces/IEditorCache.cs
@@ -8,5 +8,6 @@ namespace Negi0109.HistoryViewer.Interfaces
         public bool Exists(string label, string key);
         public void Put(string label, string key, string content);
         public string Get(string label, string key);
+        public void Get(string label, string key, Action<StreamReader> func);
     }
 }
