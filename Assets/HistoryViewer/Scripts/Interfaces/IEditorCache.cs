6bd0c8c8e3d03dc358dcd9b653bd68fb3d871aae
negi0109 <33025461+negi0109@users.noreply.github.com>
2022-10-15 15:30:38 +0900

    add EditorCache

---
+++ Assets/HistoryViewer/Scripts/Interfaces/IEditorCache.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+namespace Negi0109.HistoryViewer.Interfaces
+{
+    public interface IEditorCache
+    {
+        public bool Exists(string label, string key);
+        public void Put(string label, string key, string content);
+        public string Get(string label, string key);
+    }
+}
